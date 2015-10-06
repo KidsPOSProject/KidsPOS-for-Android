6c4e242e8ef00fc8a1cb7460d7691a2884e48f66
atsumi <atsumi@mail.bizen.jp>
2015-10-06 23:14:53 +0900

    [model] add ItemObject

--- app/src/main/java/info/nukoneko/cuc/kidspos/activity/TopPageActivity.java
+++ app/src/main/java/info/nukoneko/cuc/kidspos/activity/TopPageActivity.java
@@ -5,13 +5,23 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.Bind;
 import info.nukoneko.cuc.kidspos.R;
+import info.nukoneko.cuc.kidspos.adapter.ItemListAdapter;
 import info.nukoneko.cuc.kidspos.common.CommonActivity;
+import info.nukoneko.cuc.kidspos.model.ItemObject;
 import info.nukoneko.cuc.kidspos.navigation.NavigationView;
+import info.nukoneko.cuc.kidspos.util.KPLogger;
+import info.nukoneko.cuc.kidspos.util.KPToast;
+import info.nukoneko.cuc.kidspos.util.SQLiteManager;
 
 /**
  * created at 2015/06/13.
@@ -20,7 +30,12 @@ public class TopPageActivity extends CommonActivity {
     @Bind(R.id.tool_bar) Toolbar toolbar;
     @Bind(R.id.drawer_layout) DrawerLayout mDrawerLayout;
     @Bind(R.id.navigation_view) NavigationView mNavigation;
+    @Bind(R.id.item_list) ItemListView mItemListView;
+    @Bind(R.id.price) TextView priceView;
+
     ActionBarDrawerToggle mDrawerToggle;
+
+    Integer sumPrice = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,9 +50,8 @@ public class TopPageActivity extends CommonActivity {
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
 
-
-        mNavigation.setStoreName("もんげー");
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
@@ -53,6 +67,19 @@ public class TopPageActivity extends CommonActivity {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onInputBarcode(String barcode) {
+        ItemObject item = SQLiteManager.getItem(barcode);
+        if(item == null) {
+            KPToast.showToast("登録されていない商品です");
+        }else{
+            this.mItemListView.getAdapter().add(item);
+
+            this.sumPrice += item.price;
+            this.priceView.setText(String.valueOf(this.sumPrice));
+        }
     }
 
     private ActionBarDrawerToggle getDrawerToggle(){
--- app/src/main/java/info/nukoneko/cuc/kidspos/common/CommonActivity.java
+++ app/src/main/java/info/nukoneko/cuc/kidspos/common/CommonActivity.java
@@ -3,12 +3,16 @@ package info.nukoneko.cuc.kidspos.common;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.WindowManager;
+
+import info.nukoneko.cuc.kidspos.model.ItemObject;
+import info.nukoneko.cuc.kidspos.util.KPLogger;
 
 /**
  * created at 2015/06/13.
  */
-public class CommonActivity extends AppCompatActivity {
+abstract public class CommonActivity extends AppCompatActivity {
     String TAG = "CommonActivity";
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,38 @@ public class CommonActivity extends AppCompatActivity {
     public void onConfigurationChanged(Configuration newConfig){
         super.onConfigurationChanged(newConfig);
     }
+
+    String inputValue = "";
+    boolean flip = false;
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+            if (inputValue.length() == 13){
+                KPLogger.d("READ-END", inputValue);
+                onInputBarcode(inputValue);
+
+            }else {
+                KPLogger.d("READ-ERROR", inputValue);
+            }
+            inputValue = "";
+            return false;
+        }
+
+        int v = event.getKeyCode() - 7;
+        if (flip) {
+            inputValue += String.valueOf(v);
+            flip = false;
+        }else {
+            flip = true;
+        }
+        return super.dispatchKeyEvent(event);
+    }
+
+    protected void onInputBarcode(String barcode){
+        // Stub
+    }
+
     public String getTAG(){
         return this.TAG;
     }
