ec43a04ce392494ac7c86cb70a26a42184ed0fa2
telneko <telneko@Atsumac.local>
2017-03-03 13:39:41 +0900

    update

--- app/src/main/java/info/nukoneko/cuc/android/kidspos/util/KPPracticeTool.java
+++ app/src/main/java/info/nukoneko/cuc/android/kidspos/util/KPPracticeTool.java
@@ -8,8 +8,9 @@ import info.nukoneko.kidspos4j.model.ModelItem;
 public final class KPPracticeTool {
     public static ModelItem findModelItem(@NonNull String barcode) {
         final ModelItem item = new ModelItem();
-        item.setName("商品");
+        item.setName("しょうひん");
         item.setPrice(300);
+        item.setBarcode(barcode);
         return item;
     }
 }
