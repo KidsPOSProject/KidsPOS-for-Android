f70c9047f0c2993487e5347655d144f67502c37c
atsumi <atsumi@mail.bizen.jp>
2018-11-16 01:41:15 +0900

    refactor: modify create mock model function name.

--- app/src/main/java/info/nukoneko/cuc/android/kidspos/entity/Staff.kt
+++ app/src/main/java/info/nukoneko/cuc/android/kidspos/entity/Staff.kt
@@ -2,8 +2,8 @@ package info.nukoneko.cuc.android.kidspos.entity
 
 data class Staff(val barcode: String, val name: String) {
     companion object {
-        fun createTestObject(barcode: String): Staff {
-            return Staff(barcode, "DummyITem")
+        fun create(barcode: String): Staff {
+            return Staff(barcode, "Dummy")
         }
     }
 }
