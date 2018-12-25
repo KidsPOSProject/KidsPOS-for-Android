0b520685d2fef15a564420fd215e90146feb9e63
atsumi <atsumi@mail.bizen.jp>
2018-12-26 01:42:23 +0900

    migrate: java to kotlin.

--- app/src/main/kotlin/info/nukoneko/cuc/android/kidspos/ui/common/BaseActivity.kt
+++ app/src/main/kotlin/info/nukoneko/cuc/android/kidspos/ui/common/BaseActivity.kt
@@ -2,29 +2,9 @@ package info.nukoneko.cuc.android.kidspos.ui.common
 
 import android.support.v7.app.AppCompatActivity
 import info.nukoneko.cuc.android.kidspos.KidsPOSApplication
-import org.greenrobot.eventbus.EventBus
 
 abstract class BaseActivity : AppCompatActivity() {
-
     @Deprecated("app は ViewModel内で利用しましょう", ReplaceWith("", ""))
     protected val app: KidsPOSApplication?
         get() = KidsPOSApplication.get(this)
-
-    protected open fun shouldEventSubscribes(): Boolean {
-        return false
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        if (shouldEventSubscribes()) {
-            EventBus.getDefault().register(this)
-        }
-    }
-
-    public override fun onStop() {
-        super.onStop()
-        if (shouldEventSubscribes()) {
-            EventBus.getDefault().unregister(this)
-        }
-    }
 }
---
+++ app/src/main/kotlin/info/nukoneko/cuc/android/kidspos/ui/main/calculate/AccountResultDialogViewModel.kt
@@ -0,0 +1,44 @@
+package info.nukoneko.cuc.android.kidspos.ui.main.calculate
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
+import android.view.View
+
+class AccountResultDialogViewModel(application: Application): AndroidViewModel(application) {
+    private val price = MutableLiveData<Int>()
+    fun getPrice(): LiveData<Int> = price
+
+    private val receiveMoney = MutableLiveData<Int>()
+    fun getReceiveMoney(): LiveData<Int> = receiveMoney
+
+    var listener: Listener? = null
+
+    val chargeRiver: Int
+        get() = (receiveMoney.value ?: 0) - (price.value ?: 0)
+
+    init {
+        price.value = 0
+        receiveMoney.value = 0
+    }
+
+    fun setupValue(price: Int, receiveMoney: Int) {
+        this.price.postValue(price)
+        this.receiveMoney.postValue(receiveMoney)
+    }
+
+    fun onAccountClick(@Suppress("UNUSED_PARAMETER") view: View?) {
+        listener?.onAccount()
+    }
+
+    fun onBackClick(@Suppress("UNUSED_PARAMETER") view: View?) {
+        listener?.onBack()
+    }
+
+    interface Listener {
+        fun onAccount()
+
+        fun onBack()
+    }
+}
