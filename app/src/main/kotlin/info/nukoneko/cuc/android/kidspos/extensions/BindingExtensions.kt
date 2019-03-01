4d63e3146ca92bb4c524f862f4ee5f0f1faee4f4
atsumi <atsumi@mail.bizen.jp>
2019-03-02 07:10:34 +0900

    fix: item list cahce.

--- app/src/main/kotlin/info/nukoneko/cuc/android/kidspos/extensions/BindingExtensions.kt
+++ app/src/main/kotlin/info/nukoneko/cuc/android/kidspos/extensions/BindingExtensions.kt
@@ -1,6 +1,2 @@
 package info.nukoneko.cuc.android.kidspos.extensions
 
-import android.widget.EditText
-import androidx.databinding.BindingAdapter
-import androidx.databinding.InverseBindingAdapter
-
