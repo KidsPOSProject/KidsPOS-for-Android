f69af2e9b4bec3064b967b677a047606cd077f63
atsumi3 <atsumi@mail.bizen.jp>
2021-07-23 20:14:37 +0900

    refactor Dep.kt

--- buildSrc/src/main/java/dependencies/Dep.kt
+++ buildSrc/src/main/java/dependencies/Dep.kt
@@ -4,7 +4,7 @@ package dependencies
 object Dep {
     object GradlePlugin {
         val android = "com.android.tools.build:gradle:4.0.2"
-        val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}"
+        val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
     }
 
     object Test {
@@ -14,24 +14,22 @@ object Dep {
         val testCoreKtx = "androidx.test:core-ktx:1.3.0"
         val androidJunit4Ktx = "androidx.test.ext:junit-ktx:1.1.2"
         val archCore = "androidx.arch.core:core-testing:2.1.0"
-        val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Kotlin.coroutinesVersion}"
+        val coroutinesTest =
+            "org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.9"
 
         object Mockito {
-            private val version = "3.5.13"
-            val core = "org.mockito:mockito-core:$version"
-            val android = "org.mockito:mockito-android:$version"
+            val core = "org.mockito:mockito-core:3.5.13"
+            val android = "org.mockito:mockito-android:3.5.13"
             val kotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:+"
         }
 
         object Robolectric {
-            private val version = "4.4"
-            val client = "org.robolectric:robolectric:$version"
+            val client = "org.robolectric:robolectric:4.4"
         }
 
         object Espresso {
-            private val version = "3.3.0"
-            val core = "androidx.test.espresso:espresso-core:$version"
-            val contrib = "androidx.test.espresso:espresso-contrib:$version"
+            val core = "androidx.test.espresso:espresso-core:3.3.0"
+            val contrib = "androidx.test.espresso:espresso-contrib:3.3.0"
         }
     }
 
@@ -47,31 +45,26 @@ object Dep {
     }
 
     object Kotlin {
-        val version = "1.4.10"
-        val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib:$version"
-        val coroutinesVersion = "1.3.9"
-        val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion"
-        val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
+        val stdlibJvm = "org.jetbrains.kotlin:kotlin-stdlib:1.4.10"
+        val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9"
+        val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9"
     }
 
     object Koin {
-        private val version = "2.1.6"
-        val core = "org.koin:koin-core:$version"
-        val android = "org.koin:koin-android:$version"
-        val viewModel = "org.koin:koin-androidx-viewmodel:$version"
-        val test = "org.koin:koin-test:$version"
+        val core = "org.koin:koin-core:2.1.6"
+        val android = "org.koin:koin-android:2.1.6"
+        val viewModel = "org.koin:koin-androidx-viewmodel:2.1.6"
+        val test = "org.koin:koin-test:2.1.6"
     }
 
     object OkHttp {
-        private val version = "4.9.0"
-        val client = "com.squareup.okhttp3:okhttp:$version"
-        val mockWebServer = "com.squareup.okhttp3:mockwebserver:$version"
+        val client = "com.squareup.okhttp3:okhttp:4.9.0"
+        val mockWebServer = "com.squareup.okhttp3:mockwebserver:4.9.0"
     }
 
     object Retrofit {
-        private val version = "2.9.0"
-        val client           = "com.squareup.retrofit2:retrofit:$version"
-        val converterGson    = "com.squareup.retrofit2:converter-gson:$version"
+        val client = "com.squareup.retrofit2:retrofit:2.9.0"
+        val converterGson = "com.squareup.retrofit2:converter-gson:2.9.0"
     }
 
     object Zxing {
