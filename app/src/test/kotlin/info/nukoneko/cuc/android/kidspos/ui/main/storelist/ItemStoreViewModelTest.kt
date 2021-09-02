fabb75e9a95798f053708de6feaea2b0550ac0af
atsumi3 <atsumi@mail.bizen.jp>
2021-09-03 04:22:19 +0900

    fix test

--- app/src/test/kotlin/info/nukoneko/cuc/android/kidspos/ui/main/storelist/ItemStoreViewModelTest.kt
+++ app/src/test/kotlin/info/nukoneko/cuc/android/kidspos/ui/main/storelist/ItemStoreViewModelTest.kt
@@ -1,38 +1,43 @@
 package info.nukoneko.cuc.android.kidspos.ui.main.storelist
 
 import androidx.databinding.Observable
-import com.google.gson.Gson
 import info.nukoneko.cuc.android.kidspos.domain.entity.Store
+import info.nukoneko.cuc.android.kidspos.domain.entity.StoreId
 import info.nukoneko.cuc.android.kidspos.support.KidsPOSRobolectricTestRunner
-import junit.framework.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyInt
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import kotlin.test.assertEquals
 
 @RunWith(KidsPOSRobolectricTestRunner::class)
 class ItemStoreViewModelTest {
 
     @Test
     fun shouldGetStoreName() {
-        val store = Gson().fromJson("{\"id\": 1, \"name\": \"ぬいぐるみ\"}", info.nukoneko.cuc.android.kidspos.domain.entity.Store::class.java)
+        val store = Store(
+            id = StoreId(1),
+            name = "ぬいぐるみ"
+        )
         val viewModel = ItemStoreListContentViewModel(store, null)
         assertEquals(store.name, viewModel.storeName)
     }
 
     @Test
     fun shouldNotifyPropertyChangeWhenSetStore() {
-        val store = Gson().fromJson("{\"id\": 1, \"name\": \"ぬいぐるみ\"}", info.nukoneko.cuc.android.kidspos.domain.entity.Store::class.java)
-
+        val store = Store(
+            id = StoreId(1),
+            name = "ぬいぐるみ"
+        )
         val viewModel = ItemStoreListContentViewModel(store, null)
         val mockCallback: Observable.OnPropertyChangedCallback =
             mock(Observable.OnPropertyChangedCallback::class.java)
         viewModel.addOnPropertyChangedCallback(mockCallback)
 
         viewModel.setStore(store)
-        verify<Observable.OnPropertyChangedCallback>(mockCallback).onPropertyChanged(
+        verify(mockCallback).onPropertyChanged(
             any(Observable::class.java),
             anyInt()
         )
